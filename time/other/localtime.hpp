ec649016d079163cedbb70e32d91557165798a20
yoantie <zyz_313@163.com>
2014-07-04 10:10:34 +0800

    time

--- time/other/localtime.hpp
+++ time/other/localtime.hpp
@@ -1,25 +1,28 @@
-{
+{//C++ 函数
 	//[ wcsftime for unicode]
 	time_t t = std::time(0);
-	tm* timeinfo = std::localtime(&t);
+
+	tm* timeinfo = std::localtime(&t); //to local time
 	char str[200];
 	std::strftime(str, 200, " %b %d %X ", timeinfo);
+
+	timeinfo = std::gmtime(&t); // UTC
+
+	std::cout << std::ctime(&t) << std::endl;
+	//Converts given time since epoch to a calendar local time and then to a textual representation,
+	//as if by calling std::asctime(std::localtime(time))
+	//output:
+	//Tue Dec 27 17:21:29 2011
 }
 
-
+/*
+GetSystemTime获得的时间是基于通用协调时(UTC, Universal Time Coordinated)。
+GetLocalTime获得的时间是基于本地时间的。
+如GetLocalTime获得的本地主机的时间是上午8时，当用GetSystemTime获取时，该时间是0时，注意这两个函数的区别
+*/
+//Windows API
 SYSTEMTIME st;
 GetSystemTime(&st);
 
 SYSTEMTIME stLocalTime;
 SystemTimeToTzSpecificLocalTime(NULL, &st, &stLocalTime);
-
-#include <ctime>
-#include <iostream>
-
-int main()
-{
-    std::time_t result = std::time(NULL);
-    std::cout << std::asctime(std::localtime(&result))
-              << result << " seconds since the Epoch\n";
-}
-
